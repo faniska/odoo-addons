2a8c4cb71539418e215ad98c5b2da847b0dbc2c1
XFanis <mail@xfanis.ru>
2019-12-17 14:54:43 +0300

    Implement checking actual_mode to fix buttons visibility in edit mode

--- hide_action_buttons/static/src/js/hide_action_buttons.js
+++ hide_action_buttons/static/src/js/hide_action_buttons.js
@@ -3,7 +3,7 @@ odoo.define('hide_action_buttons.hide_action_buttons', function (require) {
     let FormView = require('web.FormView');
     FormView.include({
         load_record: function (record) {
-            if (record && this.$buttons) {
+            if (record && this.$buttons && this.get('actual_mode') === 'view') {
                 if (record.hide_action_buttons) {
                     this.$buttons.find('.o_form_buttons_view').hide();
                 } else {
